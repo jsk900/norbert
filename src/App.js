865ca69609976034eb2f020fc5c1b8e8d0f89eae
jsk900 <jsk900@yahoo.co.uk>
2018-07-19 18:30:29 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React, { PureComponent } from "react";
 
 import "./App.css";
-import Header from "./components/header";
-import getData from "./components/api";
+import Header from "./components/header.jsx";
+import getData from "./api";
 
 class App extends PureComponent {
   state = {
     data: []
+  };
+
+  renderData = () => {
+    return this.state.data.map(link => (
+      <ul>
+        <li key={link.id}>
+          <p>{link.title}</p>
+          <img
+            src={`https://farm${link.farm}.staticflickr.com/${link.server}/${
+              link.id
+            }_${link.secret}_s.jpg`}
+          />
+        </li>
+      </ul>
+    ));
   };
 
   async componentDidMount() {
@@ -20,72 +35,10 @@ class App extends PureComponent {
     return (
       <div className="main">
         <Header />
-        <div className="images">
-          {this.state.data.map(link => (
-            <ul>
-              <li key={link.id}>
-                <p>{link.title}</p>
-                <img
-                  src={`https://farm${link.farm}.staticflickr.com/${
-                    link.server
-                  }/${link.id}_${link.secret}_s.jpg`}
-                />
-              </li>
-            </ul>
-          ))}
-        </div>
+        <div className="images">{this.renderData()}</div>
       </div>
     );
   }
 }
-
-// class App extends Component {
-//   state = {
-//     picArr: [],
-//     titles: []
-//   };
-
-//   api_call = async () => {
-//     // let picArr = [];
-//     // let titles = [];
-
-//     const getPics = await fetch(`${flickr}`);
-//     const data = await getPics.json();
-//     await data.photos.photo.map(image => {
-//       // titles = titles.push(image.title);
-//       // picArr = picArr.push(
-//       let title = `${image.title}`;
-//       let srcPath = `https://farm${image.farm}.staticflickr.com/${
-//         image.server
-//       }/${image.id}_${image.secret}_s.jpg$`;
-
-//       // );
-//       // picArr.push(srcPath);
-//       this.setState({ picArr: srcPath, titles: title });
-//     });
-//   };
-
-//   componentDidMount() {
-//     this.api_call();
-//   }
-
-//   render() {
-//     console.log(this.state);
-//     return (
-//       <div className="App">
-//         <Header />
-//         <div className="images">
-//           <ul>
-//             {this.state.picArr.map(img => (
-//               <li key={img}>
-//                 <img src={img} />
-//               </li>
-//             ))}
-//           </ul>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const header = () => {
   return (
     <header className="App-header">
       <h1 className="App-title">Welcome to Norberts Site</h1>
     </header>
-  )
-}
+  );
+};
 
 export default header;
