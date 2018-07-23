90d9c054081c61220e8baf0bb17f767eee65dcb2
jsk900 <jsk900@yahoo.co.uk>
2018-07-23 21:39:24 +0200

    additions

--- src/detail.js
+++ src/detail.js
@@ -4,24 +4,38 @@ import Header from "./components/header.jsx";
 import getDescription from "./getDescription";
 
 class Detail extends PureComponent {
-  state = { data: [] };
+  state = {
+    data: [],
+    description: null,
+    farm: null,
+    server: null,
+    id: null,
+    secret: null
+  };
 
   async componentDidMount() {
     this.setState({
       data: await getDescription(this.props.location.state.id)
     });
+    this.setState({
+      description: this.state.data.photo.description._content,
+      farm: this.state.data.photo.farm,
+      server: this.state.data.photo.server,
+      id: this.state.data.photo.id,
+      secret: this.state.data.photo.secret
+    });
   }
 
   render() {
-    if (!this.state.data.length) {
-      return <p>Loading...</p>;
-    }
-
-    const description = this.state.data.photo.description._content;
-    const { farm, server, id, secret } = this.state.data.photo;
     return (
       <div>
-        <p>{id}</p>
+        <Header />
+        <p>{this.state.description}</p>
+        <img
+          src={`https://farm${this.state.farm}.staticflickr.com/${
+            this.state.server
+          }/${this.state.id}_${this.state.secret}_z.jpg`}
+        />
       </div>
     );
   }
