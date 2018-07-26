0bb92779245ebf6467304ff5ca47a4108a9f85c9
jsk900 <jsk900@yahoo.co.uk>
2018-07-26 18:28:33 +0200

    addition

--- src/getFlickrInfo.js
+++ src/getFlickrInfo.js
@@ -1,11 +1,8 @@
 const getFlickrInfo = async props => {
   //Handle call from App to get Flickr Images and call from Detail to get description
   let info;
-  if (props.api_images) {
-    info = props.api_images;
-  } else {
-    info = props;
-  }
+
+  props.api_images ? (info = props.api_images) : (info = props);
 
   const apiCall = await fetch(`${info}`);
   const data = await apiCall.json();
