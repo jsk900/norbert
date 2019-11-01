8b734589fd0f6ff15ac221c005c4e8bc8e8f73f6
jsk900 <jsk900@yahoo.co.uk>
2019-11-01 21:17:42 +0100

    'Changes'

--- src/Header.js
+++ src/Header.js
@@ -1,38 +1,69 @@
 import React from 'react';
-import { NavLink, Link } from 'react-router-dom';
+import {
+  NavLink,
+  Link
+} from 'react-router-dom';
 
 import './css/header.css';
 
 export const Header = () => {
-  return (
-    <div className="menu">
-      <Link to="/">
-        <h3>Norbert Karraß</h3>
-      </Link>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/gallery" activeStyle={{ color: '#f78b10' }}>
-              Gallery
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/about" activeStyle={{ color: '#f78b10' }}>
-              About
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/exhibitions" activeStyle={{ color: '#f78b10' }}>
-              Exhibitions
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/contact" activeStyle={{ color: '#f78b10' }}>
-              Contact
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    </div>
+  return ( <
+    div className = "menu" >
+    <
+    Link to = "/" >
+    <
+    h3 > Norbert Karraß < /h3> <
+    /Link> <
+    nav >
+    <
+    ul >
+    <
+    li >
+    <
+    NavLink to = "/gallery"
+    activeStyle = {
+      {
+        color: '#3b558f'
+      }
+    } >
+    Gallery <
+    /NavLink> <
+    /li> <
+    li >
+    <
+    NavLink to = "/about"
+    activeStyle = {
+      {
+        color: '#3b558f'
+      }
+    } >
+    About <
+    /NavLink> <
+    /li> <
+    li >
+    <
+    NavLink to = "/exhibitions"
+    activeStyle = {
+      {
+        color: '#3b558f'
+      }
+    } >
+    Exhibitions <
+    /NavLink> <
+    /li> <
+    li >
+    <
+    NavLink to = "/contact"
+    activeStyle = {
+      {
+        color: '#3b558f'
+      }
+    } >
+    Contact <
+    /NavLink> <
+    /li> <
+    /ul> <
+    /nav> <
+    /div>
   );
 };
