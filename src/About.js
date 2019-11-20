231bfbdf94d7e0ddc6bcf344c753ee19b5bcdbf8
jsk900 <jsk900@yahoo.co.uk>
2019-11-20 14:15:25 +0100

    Changes to mobile responsiveness

--- src/About.js
+++ src/About.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Header } from './Header';
-import './css/about.css';
+import React from "react";
+import { Header } from "./Header";
+import "./css/about.css";
 
 export const About = () => (
   <div className="about">
