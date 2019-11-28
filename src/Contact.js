9ff09d89fde5bcc825634641f5bc19cd881b43ee
jsk900 <jsk900@yahoo.co.uk>
2019-11-28 20:19:54 +0100

    added new contact image

--- src/Contact.js
+++ src/Contact.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Header } from './Header';
-import './css/contact.css';
+import React from "react";
+import { Header } from "./Header";
+import "./css/contact.css";
 
 export const Contact = () => (
   <div className="contact">
     <Header />
     <div className="contact_details">
-      <img src={require('./images/contact.jpg')} alt="contact" />
+      <img src={require("./images/contact.jpg")} alt="contact" />
       <p>
         <a href="mailto:norkart@gmx.de">Contact Me</a>
       </p>
