375ef8d8995200b1b34bc5cad53621c78fef16b4
jsk900 <jsk900@yahoo.co.uk>
2021-01-15 15:03:29 +0100

    added email icon

--- src/Contact.js
+++ src/Contact.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Header } from "./Header";
+import { HiOutlineMail } from 'react-icons/hi';
+
 import "./css/contact.css";
 
 export const Contact = () => (
@@ -8,7 +10,7 @@ export const Contact = () => (
     <div className="contact_details">
       <img src={require("./images/contact.jpg")} alt="contact" />
       <p>
-        <a href="mailto:norkart@gmx.de">Please contact me for pricing, or further information</a>
+        <a href="mailto:norkart@gmx.de">Please contact me for pricing, or further information <HiOutlineMail className='icon'/></a>
       </p>
     </div>
   </div>
